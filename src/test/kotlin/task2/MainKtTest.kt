003336ad30ce937d617d454d14a79b756ec6d694
SvetlanaDD <asweet.lana@gmail.com>
2022-07-09 22:02:09 +0300

    Add autotest

---
+++ src/test/kotlin/task2/MainKtTest.kt
@@ -0,0 +1,63 @@
+package task2
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun feeMoneyTransferVkpay() {
+        val cardType = "Vk Pay"
+        val totalTransfer = 0
+        val amountTransfer = 100
+
+        val result = task2.feeMoneyTransfer(cardType, totalTransfer, amountTransfer)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun feeMoneyTransferVisaLow() {
+        val cardType = "Visa"
+        val totalTransfer = 0
+        val amountTransfer = 1000 * 100
+
+        val result = task2.feeMoneyTransfer(cardType, totalTransfer, amountTransfer)
+
+        assertEquals(3500, result)
+    }
+
+    @Test
+    fun feeMoneyTransferVisaHigh() {
+        val cardType = "Visa"
+        val totalTransfer = 0
+        val amountTransfer = 10000 * 100
+
+        val result = task2.feeMoneyTransfer(cardType, totalTransfer, amountTransfer)
+
+        assertEquals(7500, result)
+    }
+
+    @Test
+    fun feeMoneyTransferMastercardLow() {
+        val cardType = "Mastercard"
+        val totalTransfer = 12000 * 100
+        val amountTransfer = 10000 * 100
+
+        val result = task2.feeMoneyTransfer(cardType, totalTransfer, amountTransfer)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun feeMoneyTransferMastercardHigh() {
+        val cardType = "Mastercard"
+        val totalTransfer = 76000 * 100
+        val amountTransfer = 1000 * 100
+
+        val result = task2.feeMoneyTransfer(cardType, totalTransfer, amountTransfer)
+
+        assertEquals(2600, result)
+    }
+}
