74d8002299d60920debfc6ced1dce299eec3bb52
SvetlanaDD <asweet.lana@gmail.com>
2022-07-09 22:01:02 +0300

    Error correction

--- src/main/kotlin/task2/main.kt
+++ src/main/kotlin/task2/main.kt
@@ -2,7 +2,7 @@ package task2
 
 fun main () {
     val cardType = "Мир"
-    val totalTransfer = 0
+    var totalTransfer = 0
     val amountTransfer = 5000 * 100 // переводим в копейки
     println("Комиссия за перевод ${feeMoneyTransfer(cardType, totalTransfer, amountTransfer) / 100} руб " +
             "${feeMoneyTransfer(cardType, totalTransfer, amountTransfer) % 100} коп")
@@ -11,7 +11,8 @@ fun main () {
 fun feeMoneyTransfer (cardType: String = "Vk Pay", totalTransfer: Int = 0, amountTransfer: Int): Int{
     when (cardType){
         "Visa", "Мир" -> return if (amountTransfer * 0.0075 < 3500) 3500 else {(amountTransfer * 0.0075).toInt()}
-        "Mastercard", "Maestro" -> return if (totalTransfer > 75000) {(amountTransfer * 0.006 + 20).toInt()} else 0
+        "Mastercard", "Maestro" -> return if (totalTransfer > 75000*100) {(amountTransfer * 0.006 + 20*100).toInt()}
+                                            else 0
         else -> return 0
     }
 }
